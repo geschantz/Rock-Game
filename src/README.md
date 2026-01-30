c0de0fefdaa713cf130b72e89f68a8fcd6c784ab
geschantz <geschantz@thewildcats.net>
2026-01-30 10:34:13 -0500

    Created project

---
+++ src/README.md
@@ -0,0 +1,52 @@
+# Overview
+Rock–Paper–Scissors–Lizard–Spock is an expanded version of the classic game, Rock-Paper-Scissors.
+
+It’s a recreation of Rock-Paper-Scissors with Lizard and Spock as additional features.
+
+The player competes against the computer.
+
+## Rules
+
+- Scissors cuts Paper
+
+- Paper covers Rock
+
+- Rock crushes Lizard
+
+- Lizard poisons Spock
+
+- Spock smashes Scissors
+
+- Scissors decapitates Lizard
+
+- Lizard eats Paper
+
+- Paper disproves Spock
+
+- Spock vaporizes Rock
+
+- Rock crushes Scissors
+
+- If both players choose the same option → Tie
+
+## Game Breakdown
+### Game Setup
+- Display welcome message
+- Explanation of the rules
+- Clear console
+
+### Simple Gameplay
+
+1. Ask the user if they want to play or not
+
+2. Randomly generate a choice for the computer from:  RO, PA, SC, LZ, SP
+
+3. Ask the player for a choice: RO, PA, SC, LZ, SP
+
+4. Counts from Rock… Paper… Scissors… Lizard… Spock
+5. Determines the winner
+6. Displays test-based graphics of the computer’s answer and the user’s answer
+Declares winner (e.g. “Scissors cuts Paper. You win!”)
+
+7. Ask the user to play again (Input “play”) or see the rules (Input “rules”)
+
---
+++ src/RockGame.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class RockGame {
+
+    public RockGame() {
+        Scanner scanner = new Scanner(System.in);
+        pregameMessages();
+        isReady();
+    }
+
+    public void pregameMessages(){
+
+        String welcomeMessage = "WELCOME TO ROCK PAPER SCISSORS LIZARD SPOCK.";
+        String rulesMessage = """
+            Rules:
+            
+            Scissors cuts Paper
+            
+            Paper covers Rock
+            
+            Rock crushes Lizard
+                
+            Lizard poisons Spock
+                
+            Spock smashes Scissors
+                
+            Scissors decapitates Lizard
+                
+            Lizard eats Paper
+                
+            Paper disproves Spock
+                
+            Spock vaporizes Rock
+                
+            Rock crushes Scissors
+
+            If both players choose the same option → Tie
+                """;
+
+        System.out.println(welcomeMessage);
+        System.out.println(rulesMessage);
+    }
+
+    public boolean isReady() {
+        String readyMessage = "Enter 'ready' when you want to play.";
+        System.out.println(readyMessage);
+        return false;
+    }
+
+    public String getValidString(String input) {
+        return "";
+    }
+}
