e8bd864c914b767fbca2b5981759c19767b078c3
geschantz <geschantz@thewildcats.net>
2026-02-06 20:09:59 -0500

    Fixed the validation at the end of the overall structure loop

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main  {
         int numOfWins = 0;
         int numOfLosses = 0;
         int numOfTies = 0;
+        int numOfRounds = 0;
         Scanner scanner = new Scanner(System.in);
         boolean playAgain = true;
         String input = null;
@@ -17,28 +18,44 @@ public class Main  {
             game.runGame();
             if (RockGame.result  == RockGame.Result.WIN) {
                 numOfWins += 1;
+                numOfRounds += 1;
 
             } else if (RockGame.result == RockGame.Result.LOSE) {
                 numOfLosses += 1;
+                numOfRounds += 1;
 
             } else if (RockGame.result == RockGame.Result.TIE) {
                 numOfTies += 1;
             }
 
-            System.out.printf("\nW: %s, L: %s, T: %s\n", numOfWins, numOfLosses, numOfTies);
+            System.out.printf("\nWins: %s, Losses: %s, Ties: %s\n", numOfWins, numOfLosses, numOfTies);
 
             System.out.println("\nWould you like to play again? (Y/N)");
             input = scanner.nextLine();
+            input = input.toUpperCase();
 
             if  (input.equals("Y")) {
                 playAgain = true;
 
             } else if (input.equals("N")) {
+                if (numOfWins > numOfLosses) {
+                    System.out.printf("\nYou won overall! You beat the computer %s times out of %s.\n",  numOfWins, numOfRounds);
+
+                } else if (numOfLosses > numOfWins) {
+                    System.out.printf("\nYou lost overall. The computer beat you  %s times out of %s.\n",  numOfLosses, numOfRounds);
+
+                } else {
+                    System.out.printf("\nYou tied overall. The score was  %s-%s.\n",  numOfWins, numOfLosses);
+
+                }
                 playAgain = false;
-                break;
 
             } else {
-                System.out.println("(Y/N)");
+                while (!input.equals("Y") && !input.equals("N")) {
+                    System.out.println("Enter Y/N");
+                    input = scanner.nextLine();
+                    input = input.toUpperCase();
+                }
             }
         }
     }
